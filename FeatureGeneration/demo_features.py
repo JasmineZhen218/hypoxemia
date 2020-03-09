b09ddc990ae70dcc85a865076c2af4ab1d6fc734
Stephen Kyranakis <stephen.kyranakis@gmail.com>
2020-03-08 22:06:05 -0400

    Feature generation updated

---
+++ FeatureGeneration/demo_features.py
@@ -0,0 +1,273 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+import scipy
+
+from feature_extraction import (
+    moving_average,
+    ema_and_emvar,
+    finite_approx_ewma_and_ewmvar,
+    linear_lstsq,
+    linear_lstsq_deriv,
+    spectral_entropy,
+    spectral_entropy_detrend
+)
+
+
+def test_features():
+    with open('SpO2_and_hypoxemia_labels/p000052-2191-01-10-02-21n.json', 'r') as json_file:
+        # data = json.load(json_file)
+        # raw_data = data['SpO2'][:500]
+        np.random.seed(0)
+        # signal = np.sin(np.arange(0, (6 * np.pi), (2 * np.pi / 50)))
+        # signal = np.sin(np.arange(0, (20 * np.pi), (2 * np.pi / 100))) + \
+        #          2 * np.sin(np.arange(0, (4 * np.pi), (2 * np.pi / 500))) + \
+        #          0.5 * np.sin(np.arange(0, (200 * np.pi), (2 * np.pi / 10)))
+        signal = np.concatenate([np.sin(np.arange(0, (10 * np.pi), (2 * np.pi / 100))),
+                                 np.sin(np.arange(0, (4 * np.pi), (2 * np.pi / 500)))])
+        # signal = np.concatenate([np.zeros(50), np.ones(50)])
+        # signal = np.concatenate([np.zeros(500), np.ones(500)])
+        # signal = np.concatenate([np.arange(0, 100), 100 * np.ones(100)])
+        # signal = np.concatenate([np.arange(0, 1000), 1000 * np.ones(1000)])
+        # signal = np.concatenate([1 - np.exp(-np.arange(0, 10, 10 / 1000)), np.ones(1000)])
+        # signal = 4 * np.arange(0, 1, 1 / 150) * (1 - np.arange(0, 1, 1 / 150))
+        # # noise_std = 4 * np.arange(0, 1, 1 / 150) * (1 - np.arange(0, 1, 1 / 150))
+        # noise_std = np.ones(150)
+        # noise_std = np.concatenate([0.09 * np.ones(50), 0.49 * np.ones(50), 0.25 * np.ones(50)])
+        # noise_std = np.concatenate([0.09 * np.ones(500), 1 * np.ones(500)])
+        # noise_std = np.concatenate([0.09 * np.ones(200), 0.49 * np.ones(200), 0.25 * np.ones(200)])
+        # noise = np.asarray([np.random.normal(0, i) for i in noise_std])
+        # raw_data = signal + noise
+        raw_data = signal
+        # raw_data = noise
+
+        # mean_5 = moving_average(raw_data, 5)
+        # mean_20 = moving_average(raw_data, 20)
+        # ewma_0_333, ewmvar_0_333 = ema_and_emvar(raw_data, 0.333)
+        # ewma_0_095, ewmvar_0_095 = ema_and_emvar(raw_data, 0.095)
+        # fin_ewma_5, fin_ewmvar_5 = finite_approx_ewma_and_ewmvar(raw_data, 5)
+        # fin_ewma_20, fin_ewmvar_20 = finite_approx_ewma_and_ewmvar(raw_data, 20)
+        lin_lst_sq_5 = linear_lstsq(raw_data, 5)
+        lin_lst_sq_20 = linear_lstsq(raw_data, 20)
+        lin_lst_sq_60 = linear_lstsq(raw_data, 60)
+        # lin_lst_sq_deriv_5 = linear_lstsq_deriv(raw_data, 5)
+        # lin_lst_sq_deriv_20 = linear_lstsq_deriv(raw_data, 20)
+        spec_entr_5 = spectral_entropy(raw_data, 5)
+        spec_entr_20 = spectral_entropy(raw_data, 20)
+        spec_entr_60 = spectral_entropy(raw_data, 60)
+        spec_entr_det_5 = spectral_entropy_detrend(raw_data, lin_lst_sq_5, 5)
+        spec_entr_det_20 = spectral_entropy_detrend(raw_data, lin_lst_sq_20, 20)
+        spec_entr_det_60 = spectral_entropy_detrend(raw_data, lin_lst_sq_60, 60)
+
+        # plt.figure(1)
+        # plt.plot(signal)
+        # plt.title('Signal')
+        # plt.xlabel('Time (min)')
+        #
+        # plt.figure(2)
+        # plt.plot(noise)
+        # plt.title('Noise')
+        # plt.xlabel('Time (min)')
+        #
+        plt.figure(3)
+        plt.plot(raw_data)
+        plt.title('Combined Data')
+        plt.xlabel('Time (min)')
+
+        # plt.figure(4)
+        # plt.plot(raw_data)
+        # plt.plot(signal)
+        # plt.plot(mean_5)
+        # plt.plot(mean_20)
+        # plt.title('Moving Average')
+        # plt.xlabel('Time (min)')
+        # plt.legend(['Input', 'Signal', 'n = 5', 'n = 20'])
+        # # plt.legend(['Signal', 'n = 5', 'n = 20'])
+        #
+        # plt.figure(5)
+        # # plt.plot(raw_data)
+        # plt.plot(signal)
+        # plt.plot(ewma_0_333)
+        # plt.plot(ewma_0_095)
+        # plt.title('Exponentially Weighted Moving Average')
+        # plt.xlabel('Time (min)')
+        # # plt.legend(['Input', 'Signal', 'alpha = 0.333 ewma', 'alpha = 0.095 ewma'])
+        # plt.legend(['Signal', 'alpha = 0.333 ewma', 'alpha = 0.095 ewma'])
+        #
+        # plt.figure(6)
+        # # plt.plot(raw_data)
+        # plt.plot(signal)
+        # plt.plot(fin_ewma_5)
+        # plt.plot(fin_ewma_20)
+        # plt.title('Finite Approximation of Exponentially Weighted Moving Average')
+        # plt.xlabel('Time (min)')
+        # # plt.legend(['Input', 'Signal', 'n = 5', 'n = 20'])
+        # plt.legend(['Signal', 'n = 5', 'n = 20'])
+        #
+        # plt.figure(7)
+        # plt.plot(raw_data)
+        # plt.plot(signal)
+        # plt.plot(ewma_0_333)
+        # plt.plot(ewma_0_095)
+        # plt.plot(fin_ewma_5)
+        # plt.plot(fin_ewma_20)
+        # plt.title('Approximation Comparison')
+        # plt.xlabel('Time (min)')
+        # plt.legend(['Input', 'Signal', 'Original, alpha = 0.333', 'Original, alpha = 0.095',
+        #             'Approximation, n = 5', 'Approximation, n = 20'])
+        # # plt.legend(['Signal', 'Original, alpha = 0.333', 'Original, alpha = 0.095',
+        # #             'Approximation, n = 5', 'Approximation, n = 20'])
+        #
+        # plt.figure(8)
+        # plt.plot(raw_data)
+        # plt.plot(mean_20)
+        # plt.plot(ewma_0_095)
+        # plt.title('Effect of Exponential Weighting')
+        # plt.xlabel('Time (min)')
+        # plt.legend(['Signal', 'Mean, n = 50', 'EWMA, alpha = 0.095'])
+        #
+        # plt.figure(9)
+        # plt.plot(raw_data)
+        # plt.plot(signal)
+        # plt.plot(lin_lst_sq_5)
+        # plt.plot(lin_lst_sq_20)
+        # plt.plot(lin_lst_sq_60)
+        # plt.title('Linear Least-Squares Filtering')
+        # plt.xlabel('Time (min)')
+        # plt.legend(['Input', 'Signal', 'n = 5', 'n = 20', 'n = 60'])
+        # # plt.legend(['Signal', 'n = 5', 'n = 20'])
+        #
+        # plt.figure(10)
+        # plt.plot(raw_data)
+        # # plt.plot(signal)
+        # plt.plot(mean_5)
+        # plt.plot(fin_ewma_5)
+        # plt.plot(lin_lst_sq_5)
+        # plt.title('Filtering Comparison, n = 5')
+        # plt.xlabel('Time (min)')
+        # # plt.legend(['Input', 'Signal', 'Moving Average', 'Approximate Exponential', 'Linear Least-Squares'])
+        # # plt.legend(['Signal', 'Moving Average', 'Approximate Exponential', 'Linear Least-Squares'])
+        # plt.legend(['Input', 'Moving Average', 'Approximate Exponential', 'Linear Least-Squares'])
+        #
+        # plt.figure(11)
+        # plt.plot(raw_data)
+        # # plt.plot(signal)
+        # plt.plot(mean_20)
+        # plt.plot(fin_ewma_20)
+        # plt.plot(lin_lst_sq_20)
+        # plt.title('Filtering Comparison, n = 20')
+        # plt.xlabel('Time (min)')
+        # # plt.legend(['Input', 'Signal', 'Moving Average', 'Approximate Exponential', 'Linear Least-Squares'])
+        # # plt.legend(['Signal', 'Moving Average', 'Approximate Exponential', 'Linear Least-Squares'])
+        # plt.legend(['Input', 'Moving Average', 'Approximate Exponential', 'Linear Least-Squares'])
+        #
+        # plt.figure(12)
+        # plt.bar(range(-(5 - 1), 1), np.ones(5) / 5)
+        # plt.title('Moving Average n = 5 Filter')
+        #
+        # plt.figure(13)
+        # plt.bar(range(-(20 - 1), 1), np.ones(20) / 20)
+        # plt.title('Moving Average n = 20 Filter')
+        #
+        # plt.figure(14)
+        # n = 100
+        # alpha = 0.333
+        # weights = alpha * np.ones(n)
+        # for i in range(n):
+        #     weights[n - i - 1] *= (1 - alpha) ** i
+        # plt.bar(range(-(n - 1), 1), weights)
+        # plt.title('EWMA alpha = 0.333 Filter')
+
+        # plt.figure(15)
+        # n = 100
+        # alpha = 0.095
+        # weights = alpha * np.ones(n)
+        # for i in range(n):
+        #     weights[n - i - 1] *= (1 - alpha) ** i
+        # plt.bar(range(-(n - 1), 1), weights)
+        # plt.title('EWMA alpha = 0.095 Filter')
+
+        # plt.figure(16)
+        # n = 5
+        # alpha = 2 / (n + 1)
+        # weights = alpha * np.ones(n)
+        # for i in range(n):
+        #     weights[n - i - 1] *= (1 - alpha) ** i
+        # weights = weights / np.sum(weights)
+        # plt.bar(range(-(n - 1), 1), weights)
+        # plt.title('Finite Approximate EWMA n = 5 Filter')
+        #
+        # plt.figure(17)
+        # n = 20
+        # alpha = 2 / (n + 1)
+        # weights = alpha * np.ones(n)
+        # for i in range(n):
+        #     weights[n - i - 1] *= (1 - alpha) ** i
+        # weights = weights / np.sum(weights)
+        # plt.bar(range(-(n - 1), 1), weights)
+        # plt.title('Finite Approximate EWMA n = 20 Filter')
+        #
+        # plt.figure(18)
+        # n = 5
+        # weights = np.asarray([(6 * k + 4 * n - 2) / (n * (n + 1)) for k in range(- n + 1, 1)])
+        # plt.bar(range(-(n - 1), 1), weights)
+        # plt.title('Linear Least-Squares n = 5 Filter')
+        #
+        # plt.figure(19)
+        # n = 20
+        # weights = np.asarray([(6 * k + 4 * n - 2) / (n * (n + 1)) for k in range(- n + 1, 1)])
+        # plt.bar(range(-(n - 1), 1), weights)
+        # plt.title('Linear Least-Squares n = 20 Filter')
+        #
+        # plt.figure(20)
+        # plt.plot(np.gradient(signal))
+        # plt.plot(lin_lst_sq_deriv_5)
+        # plt.plot(lin_lst_sq_deriv_20)
+        # plt.title('Linear Least-Squares Derivative Approximation')
+        # plt.xlabel('Time (min)')
+        # plt.legend(['Derivative of Signal', 'n = 5', 'n = 20'])
+
+        # plt.figure(21)
+        # plt.plot(np.square(noise_std))
+        # plt.plot(ewmvar_0_333)
+        # plt.plot(ewmvar_0_095)
+        # plt.title('Exponentially Weighted Moving Variance')
+        # plt.xlabel('Time (min)')
+        # plt.legend(['Noise Distribution', 'alpha = 0.333', 'alpha = 0.095'])
+        #
+        # plt.figure(22)
+        # # plt.plot(signal)
+        # plt.plot(np.square(noise_std))
+        # plt.plot(fin_ewmvar_5)
+        # plt.plot(fin_ewmvar_20)
+        # plt.title('Approximate Finite EWMV')
+        # plt.xlabel('Time (min)')
+        # plt.legend(['Noise Distribution', 'n = 5', 'n = 20'])
+
+        plt.figure(23)
+        # plt.plot(signal)
+        # plt.plot(np.square(noise_std))
+        plt.plot(spec_entr_5)
+        plt.plot(spec_entr_20)
+        plt.plot(spec_entr_60)
+        plt.title('Spectral Entropy')
+        plt.xlabel('Time (min)')
+        plt.legend(['n = 5', 'n = 20', 'n = 60'])
+        plt.ylim(0)
+
+        plt.figure(24)
+        # plt.plot(signal)
+        # plt.plot(np.square(noise_std))
+        plt.plot(spec_entr_det_5)
+        plt.plot(spec_entr_det_20)
+        plt.plot(spec_entr_det_60)
+        plt.title('Spectral Entropy (Detrended)')
+        plt.xlabel('Time (min)')
+        plt.legend(['n = 5', 'n = 20', 'n = 60'])
+        plt.ylim(0)
+
+        plt.show()
+
+
+if __name__ == '__main__':
+    test_features()
--- FeatureGeneration/feature_extraction.py
+++ FeatureGeneration/feature_extraction.py
@@ -1,54 +1,20 @@
+"""Contains functions for feature extraction.
+
+Functions:
+    linear_lstsq - Linear least-squares filtering
+    linear_lstsq_deriv - Linear least-squares derivative approximation
+    finite_approx_ewma_and_ewmvar - Finite-window approximations of exponentially weighted moving average and variance
+    spectral_entropy - Spectral entropy of window of data
+    moving_average - Moving average of data (not recommended)
+    ema_and_emvar - Exponentially weighted moving average and variance
+"""
+
 import numpy as np
-import json
-import matplotlib.pyplot as plt
 import math
+import warnings
+import matplotlib.pyplot as plt
 
 from scipy import signal
-from scipy.fftpack import fft
-
-
-def moving_average(data, n):
-    """Compute the causal moving average of 1D data.
-
-    At the beginning of the data, where there is not enough previous data to do the calculation, the output is padded
-    with nans.
-
-    :param data: The data.
-    :param n: The window size of the moving average.
-    :return: The moving average, padded with nans in the front.
-    """
-    return np.concatenate([np.ones(n - 1) * np.nan, np.convolve(data, np.ones(n), mode='valid') / n])
-
-
-def ema_and_emvar(data, alpha):
-    """Compute the exponentially weighted moving average and variance.
-
-    This function handles nans in the input by carrying forward the mean and variance of the last non-nan data point as
-    the most recent mean and variance. We might instead want to start over when a nan occurs - this is something to ask
-    the engineering PIs about.
-    TODO: Ask PIs about nan issue above.
-
-    :param data: The data.
-    :param alpha: The parameter for exponential weighting.
-    :return: ema, the exponentially weighted moving average, and emvar, the exponentially weighted moving variance.
-    """
-    ema = np.zeros_like(data)
-    emvar = np.zeros_like(data)
-    ema[0] = data[0]
-    last_valid_ema = ema[0]
-    last_valid_emvar = emvar[0]
-    for i, cur in enumerate(data[1:]):
-        i = i + 1
-        if math.isnan(cur):
-            ema[i] = np.nan
-            emvar[i] = np.nan
-        else:
-            delta = cur - last_valid_ema
-            ema[i] = last_valid_ema + alpha * delta
-            emvar[i] = (1 - alpha) * (last_valid_emvar + alpha * delta ** 2)
-            last_valid_ema = ema[i]
-            last_valid_emvar = emvar[i]
-    return ema, emvar
 
 
 def linear_lstsq(data, n):
@@ -79,111 +45,97 @@ def linear_lstsq_deriv(data, n):
     return np.concatenate([np.ones(min(n - 1, len(data))) * np.nan, np.correlate(data, weights, mode='valid')])
 
 
-# def spectral_entropy(data, nfft):
-#     f, t, Zxx = signal.stft(data, window='blackmanharris', nperseg=nfft, detrend='linear')
-#     power_spectra = np.square(np.absolute(Zxx))
-#     normed_ps = power_spectrum / np.sum(power_spectrum)
-#     entropy = -np.sum(np.multiply(normed_ps, np.log2(normed_ps)) / np.log2(nfft))
-#     return entropy
+def finite_approx_ewma_and_ewmvar(data, n):
+    """Approximates the exponentially weighted moving average and variance using a window of finite length.
 
-
-def extract_features(raw_name):
-    """Extract additional features for a particular feature.
-
-    Assumes that the current folder has a subfolder names "Raw" with the data in it and one called "Features" to which
-    the features will be saved.
-
-    :param raw_name: The name of the original feature.
+    :param data: The data to extract the features from.
+    :param n: The window size
+    :return: ewma, the approximate exponentially weighted moving average, and ewmvar, the approximate exponentially
+        weighted moving variance.
     """
-    save_path = raw_name + '_Features/'
-    # save_path = 'Demo/'
-    num_records = 0
-    all_data = dict()
-    with open('Raw/' + raw_name + '.json', 'r') as json_file:
-        data = json.load(json_file)
-        print('Finished Reading')
-        num_entries = len(data)
-        for name, raw_data in data.items():
-            if num_records % 500 == 0:
-                print('{} of {}'.format(num_records, num_entries))
-            num_records += 1
-            try:
-                mean_5 = moving_average(raw_data, 5)
-                mean_20 = moving_average(raw_data, 20)
-                mean_60 = moving_average(raw_data, 60)
-                # ewma_0_2 = ewma_vectorized_safe(raw_data, 0.2)
-                # ewma_0_1 = ewma_vectorized_safe(raw_data, 0.1)
-                # ewma_0_05 = ewma_vectorized_safe(raw_data, 0.05)
-                # ewma_0_01 = ewma_vectorized_safe(raw_data, 0.01)
-                ewma_0_2, ewmvar_0_2 = ema_and_emvar(raw_data, 0.2)
-                ewma_0_1, ewmvar_0_1 = ema_and_emvar(raw_data, 0.1)
-                ewma_0_05, ewmvar_0_05 = ema_and_emvar(raw_data, 0.05)
-                ewma_0_01, ewmvar_0_01 = ema_and_emvar(raw_data, 0.01)
-                lin_lstsq_5 = linear_lstsq(raw_data, 5)
-                lin_lstsq_20 = linear_lstsq(raw_data, 20)
-                lin_lstsq_60 = linear_lstsq(raw_data, 60)
-                lin_lstsq_deriv_5 = linear_lstsq_deriv(raw_data, 5)
-                lin_lstsq_deriv_20 = linear_lstsq_deriv(raw_data, 20)
-                lin_lstsq_deriv_60 = linear_lstsq_deriv(raw_data, 60)
-                # spec_ent_20 = spectral_entropy(raw_data, 20)
-                # spec_ent_60 = spectral_entropy(raw_data, 60)
-            except:
-                continue
-            dtype = np.float32
-            data_2 = {
-                'mean_5': mean_5.astype(dtype).tolist(),
-                'mean_20': mean_20.astype(dtype).tolist(),
-                'mean_60': mean_60.astype(dtype).tolist(),
-                'ewma_0_2': ewma_0_2.astype(dtype).tolist(),
-                'ewma_0_1': ewma_0_1.astype(dtype).tolist(),
-                'ewma_0_05': ewma_0_05.astype(dtype).tolist(),
-                'ewma_0_01': ewma_0_01.astype(dtype).tolist(),
-                'ewmvar_0_2': ewmvar_0_2.astype(dtype).tolist(),
-                'ewmvar_0_1': ewmvar_0_1.astype(dtype).tolist(),
-                'ewmvar_0_05': ewmvar_0_05.astype(dtype).tolist(),
-                'ewmvar_0_01': ewmvar_0_01.astype(dtype).tolist(),
-                'lin_lstsq_5': lin_lstsq_5.astype(dtype).tolist(),
-                'lin_lstsq_20': lin_lstsq_20.astype(dtype).tolist(),
-                'lin_lstsq_60': lin_lstsq_60.astype(dtype).tolist(),
-                'lin_lstsq_deriv_5': lin_lstsq_deriv_5.astype(dtype).tolist(),
-                'lin_lstsq_deriv_20': lin_lstsq_deriv_20.astype(dtype).tolist(),
-                'lin_lstsq_deriv_60': lin_lstsq_deriv_60.astype(dtype).tolist(),
-                # 'spec_ent_20': spec_ent_20.astype(dtype).tolist(),
-                # 'spec_ent_60': spec_ent_60.astype(dtype).tolist(),
-            }
-            all_data[name] = data_2
-            if name == 'p000052-2191-01-10-02-21n':
-                with open(save_path + name + '.json', 'w') as savefile:
-                    json.dump(data_2, savefile)
-            # with open(save_path + name + '.json', 'w') as savefile:
-            #     json.dump(data_2, savefile)
-    with open('Features/' + raw_name + '_Features.json', 'w') as savefile:
-        print('About to write')
-        json.dump(all_data, savefile)
+    alpha = 2 / (n + 1)
+    weights = alpha * np.ones(n)
+    for i in range(n):
+        weights[n - i - 1] *= (1 - alpha) ** i
+    weights = weights / np.sum(weights)
+    ewma = np.concatenate([np.ones(n - 1) * np.nan, np.correlate(data, weights, mode='valid')])
+    ewmvar = np.concatenate([np.ones(n - 1) * np.nan, np.correlate(np.square(data - ewma), weights, mode='valid')])
+    return ewma, ewmvar
 
 
-def test_features():
-    with open('SpO2_and_hypoxemia_labels/p000052-2191-01-10-02-21n.json', 'r') as json_file:
-        data = json.load(json_file)
-        raw_data = data['SpO2'][:500]
-        # lin_lst_sq = linear_lstsq(raw_data, 5)
-        # lin_lst_sq = linear_lstsq(raw_data, 10)
-        # lin_lst_sq = linear_lstsq(raw_data, 13)
-        # lin_lst_sq_deriv = linear_lstsq_deriv(raw_data, 5)
-        # lin_lst_sq_deriv = linear_lstsq_deriv(raw_data, 10)
-        # lin_lst_sq_deriv = linear_lstsq_deriv(raw_data, 13)
-        ewma_0_2, ewmvar_0_2 = ema_and_emvar(raw_data, 0.2)
-        ewma_0_05, ewmvar_0_05 = ema_and_emvar(raw_data, 0.05)
+def spectral_entropy(data, n):
+    """Computes the windowed real-time spectral entropy of the data.
 
-        plt.figure()
-        plt.plot(raw_data)
-        plt.plot(ewma_0_2)
-        plt.plot(ewma_0_05)
-        plt.xlabel('Time (min)')
-        plt.legend(['SpO2', 'alpha = 0.2 moving average', 'alpha = 0.05 moving average'])
-        plt.show()
+    Note: This function does NOT detrend. Any detrending/debiasing must be done ahead of time.
+    TODO: Ensure that this works properly.
+
+    :param data: The data to extract the entropy from.
+    :param n: The size of the window.
+    :return: The spectral entropy.
+    """
+    num_nans = np.argwhere(~np.isnan(data))[0][0]
+    f, t, Zxx = signal.stft(data[num_nans:],
+                            window='blackmanharris',
+                            nperseg=n,
+                            noverlap=n - 1,
+                            detrend=False,
+                            boundary=None)
+    power_spectra = np.square(np.absolute(Zxx))
+    epsilon = 1e-30
+    normed_ps = power_spectra / (np.sum(power_spectra, axis=0) + epsilon)
+    return np.concatenate([np.ones(n - 1 + num_nans) * np.nan,
+                           -np.sum(np.multiply(normed_ps, np.log2(normed_ps + epsilon)), axis=0) / np.log2(n)])
 
 
-if __name__ == '__main__':
-    # extract_features('SpO2')
-    test_features()
+def spectral_entropy_detrend(data, filtered, n):
+    return spectral_entropy(data - filtered, n)
+
+
+def moving_average(data, n):
+    """Compute the causal moving average of 1D data.
+
+    At the beginning of the data, where there is not enough previous data to do the calculation, the output is padded
+    with nans.
+
+    NOTE: This function is NOT recommended for feature extraction, since it is linearly dependent with linear least
+        squares filtering and linear least squares derivative approximation.
+
+    :param data: The data.
+    :param n: The window size of the moving average.
+    :return: The moving average, padded with nans in the front.
+    """
+    warnings.warn("The moving average is not recommended for feature extraction.")
+    return np.concatenate([np.ones(n - 1) * np.nan, np.convolve(data, np.ones(n), mode='valid') / n])
+
+
+def ema_and_emvar(data, alpha):
+    """Compute the exponentially weighted moving average and variance.
+
+    This function handles nans in the input by carrying forward the mean and variance of the last non-nan data point as
+    the most recent mean and variance. We might instead want to start over when a nan occurs - this is something to ask
+    the engineering PIs about.
+
+    NOTE: This function is NOT recommended for feature extraction, since it uses an unbounded window.
+
+    :param data: The data.
+    :param alpha: The parameter for exponential weighting.
+    :return: ema, the exponentially weighted moving average, and emvar, the exponentially weighted moving variance.
+    """
+    warnings.warn("Unbounded exponentially weighted features are not recommended for feature extraction.")
+    ema = np.zeros_like(data)
+    emvar = np.zeros_like(data)
+    ema[0] = data[0]
+    last_valid_ema = ema[0]
+    last_valid_emvar = emvar[0]
+    for i, cur in enumerate(data[1:]):
+        i = i + 1
+        if math.isnan(cur):
+            ema[i] = np.nan
+            emvar[i] = np.nan
+        else:
+            delta = cur - last_valid_ema
+            ema[i] = last_valid_ema + alpha * delta
+            emvar[i] = (1 - alpha) * (last_valid_emvar + alpha * delta ** 2)
+            last_valid_ema = ema[i]
+            last_valid_emvar = emvar[i]
+    return ema, emvar
