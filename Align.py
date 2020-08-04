13edc2ce29f8e2e3ee0a36a8c6c9c4118a536fb4
BigGiava <54903219+BigGiava@users.noreply.github.com>
2020-08-04 16:04:08 -0400

    Align ventilation to records

--- Align.py
+++ Align.py
@@ -1,72 +1,81 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sat Jan 11 08:05:21 2020
-
-@author: Jasmine
+Modified for ventilation alignment: 7/29/2020
+Wen Shi
 """
 
 import datetime as dt
 import pandas as pd
 import numpy as np
+from datetime import timedelta
 
 
-# df1=pd.read_csv('/...csv') # sparse data to align
-# df1.set_index('subject_id',inplace=True) 
-# df2=pd.read_csv('meta_records.csv') # csv of information for each record
-
-def align_sparse(ID,record_id,start,end,sparse):
-    start_dt=dt.datetime.strptime(start,'%Y/%m/%d %H:%M')
-    end_dt=dt.datetime.strptime(end,'%Y/%m/%d %H:%M')
-    length=int((end_dt-start_dt).total_seconds()/60)
-    indexes=[]
-    value=[]
-    for i, row in sparse.iterrows():
-        charttime=row['charttime']
-        charttime_dt=dt.datetime.strptime(charttime,'%Y-%m-%d %H:%M:%S')
-        index_dt=charttime_dt-start_dt
-        index=int(index_dt.total_seconds()/60)
-        if (index>=-30) & (index<=length+30):
-            indexes.append(index)
-            value.append(row['value'])
-    subject_id=[ID]*len(indexes)
-    s=pd.DataFrame({
-            'record_id':record_id,
-            'subject_id':subject_id,
-            'length':length,
-            'time':indexes,
-            'Analyzed FiO2':value      
-                })
-    return s
-
-def s_to_df(s):
-    df=pd.DataFrame(s.values.reshape(1,-1),columns=s.index)
-    return df
-    
-
-sum=0
-for index,row in df2.iterrows():
-    ID=row['subject_id']
-    if ID not in df1.index:
+df1=pd.read_csv('noninvavent.csv') # noninvavent
+#df1=pd.read_csv('nippvvent.csv') # nippv
+#df1=pd.read_csv('target_inva_vent_sparse.csv') # invavent
+df2=pd.read_csv('events.csv') # csv of information for each record
+#%% store subid and corresponding stayid
+table = pd.read_csv('D:\mcsv\ICUSTAYS.csv')
+sub = table['SUBJECT_ID']
+stay = table['ICUSTAY_ID']
+staydic = {}
+leng = sub.count()
+for i in range(leng):
+    subid = str(sub[i])
+    stayid = str(stay[i])
+    staydic[stayid] = subid   
+#%% store subid into df1 according to staydic
+df1stays = df1['icustay_id']
+df1subs = []
+for i in df1stays:
+    df1subs.append(staydic[str(i)])
+df1['sub_id'] = df1subs
+#%% unique patient id in df2
+patients = df2['patient_ID']
+patients = np.unique(patients)
+#%% Loop through each row of df1 and df2
+ventcount = 0
+totalventminutes = 0
+ventrectable = pd.DataFrame({'subject_id':[],
+                             'vent_start_time':[],
+                             'vent_duration_min':[],
+                             'vent_end_time':[]})
+for index,row in df1.iterrows():
+    subject = int(row['sub_id'])
+    if subject not in patients:
         continue
-    record_id=row['record_id']
-    start=row['start']
-    end=row['end']
-    sparse=df1.loc[ID]
-    if type(sparse)==pd.Series:
-        sparse=s_to_df(sparse)
-    f=align_sparse(ID,record_id,start,end,sparse)
-    if not f.empty:
-        if sum==0:
-            df=f
-            sum=sum+1
-        else:
-            df=df.append(f)
-            
-df=df.sort_values(['subject_id','record_id','time'])
-
-# df.to_csv("/...csv") # save the aligned sparse data
-
-
-
-            
-
+    ventcount += 1
+    ventstarttime = row['starttime']
+    ############################Change when switching to invasive vent
+    ventduration = row['ventduration']
+    #ventduration = row['duration_hours']
+    ventstarttime_dt = dt.datetime.strptime(ventstarttime, '%Y-%m-%d %H:%M:%S')
+    #ventstarttime_dt = dt.datetime.strptime(ventstarttime, '%m/%d/%Y %H:%M')
+    ventduration_minute = int(ventduration * 60)
+    ventendtime_dt = ventstarttime_dt + timedelta(minutes = ventduration_minute)
+    for rec_index,rec_row in df2.iterrows():
+        rec_subject = int(rec_row['patient_ID'])
+        if rec_subject != subject:
+            continue
+        rec_time = rec_row['record_name'][8:-1]
+        rec_time_dt = dt.datetime.strptime(rec_time, '%Y-%m-%d-%H-%M')
+        rec_start_interval = int(rec_row['start_interval'])
+        rec_starttime_dt = rec_time_dt + timedelta(minutes = rec_start_interval)
+        rec_duration = int(rec_row['duration_interval'])
+        rec_endtime_dt = rec_starttime_dt + timedelta(minutes = rec_duration)
+        # Calculate overlap between the two time period
+        t = [(ventstarttime_dt,ventendtime_dt),(rec_starttime_dt,rec_endtime_dt)]
+        overlap = (min(t[0][1],t[1][1]) - max(t[0][0], t[1][0]))
+        if overlap.total_seconds() <= 0:
+            continue
+        ventrecstarttime = max(t[0][0], t[1][0])
+        ventrecendtime = min(t[0][1],t[1][1])
+        ventrecduration = int((ventrecendtime-ventrecstarttime).total_seconds()/60)
+        totalventminutes += ventrecduration
+        ventrectable = ventrectable.append({'subject_id':rec_subject,
+                             'vent_start_time':ventrecstarttime,
+                             'vent_duration_min':ventrecduration,
+                             'vent_end_time':ventrecendtime}, ignore_index=True)
+print(totalventminutes)
+#%% Output CSV
+ventrectable.to_csv('nippv_vent_events.csv', index=False, sep = ',')
